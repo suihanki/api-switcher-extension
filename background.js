628273de5b678c27e66c2cbb46503d9fae28904e
Brendan <brendan@pinterest.com>
2015-10-30 14:14:13 -0700

    Added right click to open pin functionality

--- background.js
+++ background.js
@@ -1,3 +1,23 @@
+// A generic onclick callback function.
+function openPinOnClick(info, tab) {
+  alert("Here is the text: "+info.selectionText);
+  var newURL = "https://www.pinterest.com/pin/"+info.selectionText;
+  chrome.tabs.create({ url: newURL });
+  console.log("item " + info.menuItemId + " was clicked");
+  console.log("info: " + JSON.stringify(info));
+  console.log("tab: " + JSON.stringify(tab));
+}
+
+// Create one test item for each context type.
+var contexts = ["selection"];
+for (var i = 0; i < contexts.length; i++) {
+  var context = contexts[i];
+  var title = "Open pin from selection";
+  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                       "onclick": openPinOnClick});
+  console.log("'" + context + "' item:" + id);
+}
+
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([
