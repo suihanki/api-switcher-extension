b67bd93b3982f710273845294de9472320132346
Brendan <brendan@pinterest.com>
2015-10-30 15:13:42 -0700

    Fixed bugs added support for www.pinterest.com

--- background.js
+++ background.js
@@ -1,18 +1,3 @@
-// A generic onclick callback function.
-function openPinOnClick(info, tab) {
-  var newURL = "https://www.pinterest.com/pin/"+info.selectionText;
-  chrome.tabs.create({ url: newURL });
-}
-
-// Create one test item for each context type.
-var contexts = ["selection"];
-for (var i = 0; i < contexts.length; i++) {
-  var context = contexts[i];
-  var title = "Open pin from selection";
-  var id = chrome.contextMenus.create({"title": title, "contexts":[context],
-                                       "onclick": openPinOnClick});
-}
-
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
 		chrome.declarativeContent.onPageChanged.addRules([
@@ -41,12 +26,37 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.pageAction.onClicked.addListener(function(tab){
 	var username = 'brendan';
 	var url = tab.url;
-	var host = tab.url.split(".com/")[0];
-	var params = tab.url.split(".com/")[1];
+	var split = tab.url.split(".com/");
+
+	var host = split[0];
+	var params = split.slice(1);
+	var joined_params = params.join(".com/");
+	
 	if(host == 'https://api.pinterest'){
-		chrome.tabs.update(tab.id, {url: "https://api-"+username+".pinterdev.com/" + params});
+		chrome.tabs.update(tab.id, {url: "https://api-"+username+".pinterdev.com/" + joined_params});
 	}
 	if(host == 'https://api-'+username+'.pinterdev'){
-		chrome.tabs.update(tab.id, {url: "https://api.pinterest.com/" + params});
+		chrome.tabs.update(tab.id, {url: "https://api.pinterest.com/" + joined_params});
+	}
+	if(host == 'https://www.pinterest'){
+		chrome.tabs.update(tab.id, {url: "https://"+username+".pinterdev.com/" + joined_params});
+	}
+	if(host == 'https://'+username+'.pinterdev'){
+		chrome.tabs.update(tab.id, {url: "https://www.pinterest.com/" + joined_params});
 	}
 });
+
+// A generic onclick callback function
+function openPinOnClick(info, tab) {
+	var newURL = "https://www.pinterest.com/pin/"+info.selectionText;
+	chrome.tabs.create({ url: newURL });
+}
+
+// Create one test item for each context type.
+var contexts = ["selection"];
+for (var i = 0; i < contexts.length; i++) {
+	var context = contexts[i];
+	var title = "Open pin from selection";
+	var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+	                                   "onclick": openPinOnClick});
+}
